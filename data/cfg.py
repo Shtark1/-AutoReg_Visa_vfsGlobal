4c157e457e227bf63ea1ae106d72da959af3f9ed
sergg <95623529+Shtark1@users.noreply.github.com>
2022-10-29 21:17:40 +0300

    Update cfg.py

--- data/cfg.py
+++ data/cfg.py
@@ -1,7 +1,7 @@
 url = "https://row2.vfsglobal.com/PolandBelarus-Appointment/Account/RegisteredLogin?q=shSA0YnE4pLF9Xzwon/x/ASnHZRMROGDyz5YljrTPrmD7weWKDzHm/9+x4kyou3T7EbygDK+7ECJT8O+dWpxGw=="
 xPATH = "//*[@href='/PolandBelarus-Appointment/Home/SelectVAC?q=dePiaPfL2MJ7yDPEmQRU6fRZbx3aIpSal6PdG3Bxqq7rSNU6HabciCVot9dEwkhd']"
 
-rucaptcha_key = "b5868793f7b53b426f53d367f7cf78d7"
-site_key = "6Ld-Kg8UAAAAAK6U2Ur94LX8-Agew_jk1pQ3meJ1"
-hsite_key = "33f96e6a-38cd-421b-bb68-7806e1764460"
+rucaptcha_key = ""
+site_key = ""
+hsite_key = ""
 
